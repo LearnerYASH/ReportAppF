696ce95398859cba2bd8504e9f2e284ad81bd367
LearnerYASH <goelyash879147@gmail.com>
2024-11-21 19:01:41 +0530

    zzzz

--- src/views/api.js
+++ src/views/api.js
@@ -1,10 +1,10 @@
-// src/services/api.js
+
 import axios from 'axios';
 import { BASE_URL } from '../config/constant';
 
 // Create an Axios instance with headers for CORS support
 const api = axios.create({
-  baseURL: 'https://spontaneous-figolla-0f6ed4.netlify.app', // Your API base URL
+  baseURL: 'https://spontaneous-figolla-0f6ed4.netlify.app/', // Your API base URL
   headers: {
     'Content-Type': 'application/json', // Ensure the content type is set to JSON
     'Access-Control-Allow-Origin': '*', // Allow all origins
