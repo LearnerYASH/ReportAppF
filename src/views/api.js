3aab1b6e7f9e88ff5da58489961f6f245aaf091b
LearnerYASH <goelyash879147@gmail.com>
2024-11-21 21:01:28 +0530

    ghhvghg

--- src/views/api.js
+++ src/views/api.js
@@ -1,16 +1,13 @@
-
 import axios from 'axios';
-import { BASE_URL } from '../config/constant';
 
-// Create an Axios instance with headers for CORS support
+
+// Create an Axios instance
 const api = axios.create({
-  baseURL: 'https://spontaneous-figolla-0f6ed4.netlify.app', // Your API base URL
+  baseURL: 'https://reportappb.onrender.com', // Your API base URL
   headers: {
-    'Content-Type': 'application/json', // Ensure the content type is set to JSON
-    'Access-Control-Allow-Origin': '*', // Allow all origins
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Allow necessary HTTP methods
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allow necessary headers
+    'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 export default api;
