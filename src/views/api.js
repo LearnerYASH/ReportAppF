fe9d48d7cdb0ff8551934c712149d2239e6f99d6
LearnerYASH <goelyash879147@gmail.com>
2024-11-21 15:39:19 +0530

    zzz

--- src/views/api.js
+++ src/views/api.js
@@ -8,20 +8,7 @@ const api = axios.create({
 });
 
 // Add a request interceptor to include the token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
 
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
 
