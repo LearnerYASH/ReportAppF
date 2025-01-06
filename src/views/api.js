eaf8597dd6eba9f0c6b8deb9c9d306a1e6250aa4
LearnerYASH <goelyash879147@gmail.com>
2025-01-07 04:04:20 +0530

    hbvjh

--- src/views/api.js
+++ src/views/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // Your API base URL
+  baseURL: 'https://reportappb.onrender.com', // Your API base URL
   headers: {
     'Content-Type': 'application/json',
   },
